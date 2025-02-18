e47b0fe4d21e533e7d38dffe9327aa6566695e41
Alexandr <alexprokopchuk2007@gmail.com>
2025-02-18 11:18:21 +0000

    first commit

---
+++ homework_1/task_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+int main() {
+
+    char name[31]; 
+   
+    std::cout << "Enter the name: " << std::endl;
+    std::cin >> name;
+    std::cout << "Hello " << name << "!" << std::endl;
+
+
+
+    return 0;
+
+
+}
